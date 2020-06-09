4b422354d2f663e87ec8f8a93ae0ea476ce6e8e0
lacsap24 <paskal.pandey@gmail.com>
2020-06-08 21:25:01 -0400

    1st

---
+++ index.js
@@ -0,0 +1,30 @@
+// let sports = ['NFL', 'NBA', 'Baseball']
+
+// console.log(sports.length)
+// console.log(sports[0])
+// let container = document.querySelector("#container")
+// let message
+// for (let i = 0; i < sports.length; i++ ){
+
+//  message = document.createElement('p')
+
+// message.innerHTML = sports[i]
+
+// container.appendChild(message)
+
+// }
+
+ 
+
+
+// // let container = document.querySelector("#container")
+
+// // let message = document.createElement('p')
+
+// // message.innerHTML = "Hello World"
+
+// // container.appendChild(message)
+
+
+console.log('Hey There')
+
